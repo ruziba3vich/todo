6e5d758a786ec672ff4a9ecbd1faedcdb3adf643
prodonik <rruzibayevich@gmail.com>
2024-04-18 08:10:24 +0500

    forgot to commit changes in `uy_ishi`

--- uy_ishi/main.go
+++ uy_ishi/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
 func main() {
-	
+	sourceFileName := "rawdata.txt"
+
+	sourceFile, err := os.Open(sourceFileName)
+	if err != nil {
+		fmt.Println("Error opening source file:", err)
+		return
+	}
+	defer sourceFile.Close()
+
+	backupFileName := generateBackupFileName(sourceFileName)
+
+	backupFile, err := os.Create(backupFileName)
+	if err != nil {
+		fmt.Println("Error creating backup file:", err)
+		return
+	}
+	defer backupFile.Close()
+
+	_, err = io.Copy(backupFile, sourceFile)
+	if err != nil {
+		fmt.Println("Error copying file:", err)
+		return
+	}
+
+	fmt.Printf("Backup file successfully created: %s\n", backupFileName)
+}
+
+func generateBackupFileName(sourceFileName string) string {
+	currentTime := time.Now()
+
+	dateString := currentTime.Format("2006-01-02")
+
+	timeString := currentTime.Format("150405")
+
+	extension := filepath.Ext(sourceFileName)
+
+	backupFileName := fmt.Sprintf("%s_backup_%s_%s%s", sourceFileName[:len(sourceFileName)-len(extension)], dateString, timeString, extension)
+
+	return backupFileName
 }
